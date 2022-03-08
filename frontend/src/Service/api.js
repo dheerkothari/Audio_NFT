ef0284c89885e8a0b66e8a89dd5d86dfc2706262
Baldev Parmar <baldev@webcluesinfotech.com>
2022-03-08 18:27:05 +0530

    changes

--- frontend/src/Service/api.js
+++ frontend/src/Service/api.js
@@ -35,3 +35,51 @@ export const addGenres = async (data) => {
         console.log("Error while addGenres API", err)
     }
 }
+
+export const getAllGenres = async (param) => {
+    try {
+        let response = await axios.get(`${URL}/genres${param}`);
+        return response.data;
+    }
+    catch (err) {
+        console.log("Error while getAllGenres API", err)
+    }
+}
+
+export const getGenre = async (id) => {
+    try {
+        let response = await axios.get(`${URL}/genre/${id}`)
+        console.log('id from api', response.data)
+        return response.data;
+    }
+    catch (err) {
+        console.log("Error while getGenre API", err)
+    }
+}
+
+export const updateGenre = async (id, post) => {
+    try {
+        await axios.post(`${URL}/update/${id}`, post)
+    }
+    catch (err) {
+        console.log("Error while updateGenre API", err)
+    }
+}
+
+export const deleteGenre = async (id) => {
+    try {
+        return await axios.delete(`${URL}/delete/${id}`)
+    }
+    catch (err) {
+        console.log("Error while deleteGenre API", err)
+    }
+}
+
+export const uploadFile = async (data) => {
+    try {
+        return await axios.post(`${URL}/file/upload`, data)
+    }
+    catch (err) {
+        console.log("Error while uploadFile API", err)
+    }
+}
